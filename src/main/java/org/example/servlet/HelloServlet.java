f30c325262a1bb0a68b443562aa423fab81b972c
Yogesh Seralia <yogeshseralia21@gmail.com>
2025-01-02 23:35:01 +0400

    first commit

---
+++ src/main/java/org/example/servlet/HelloServlet.java
@@ -0,0 +1,16 @@
+package org.example.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.getWriter().write("<h1>Hello, World!</h1>");
+    }
+}
---
+++ src/main/web/index.jsp
@@ -0,0 +1,11 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My JSP Page</title>
+</head>
+<body>
+    <h1>Welcome to JSP</h1>
+    <a href="hello">Click here for servlet</a>
+</body>
+</html>
